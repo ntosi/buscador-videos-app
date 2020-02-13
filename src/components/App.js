b4f3892fd7e24a49901f72918a2cacdeeb21d756
Nico <nicolas.tosi@gmail.com>
2020-02-13 01:50:09 -0300

    estructura basica

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Clock from './Clock'
+
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                hola
+            <Clock />    
+            </div>
+        )
+    }
+}
+
+
+
+export default App
